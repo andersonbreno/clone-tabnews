88dee3811e0a7578b2ae2cae88b6b396a39aa007
anderson.azevedo <andersonbrenotech@gmail.com>
2026-03-19 00:01:54 -0300

    ideia inicial para pagina da causa animal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-  return <h1>Tenho 27 gatões!</h1>;
+  return (
+    <h1>
+      Tenho 27 gatões! Buscar maneira de ajudar a causa animal com publicações
+      que ajudem na concientização sobre a importância de adotar animais de rua,
+      compartilhar histórias de sucesso de adoção e fornecer informações sobre
+      cuidados com os animais. Além disso, posso criar conteúdo que destaque a
+      importância da castração para controlar a população de animais de rua.
+      Posso também colaborar com organizações de resgate animal, promovendo
+      eventos de adoção e arrecadação de fundos para ajudar os animais
+      necessitados.
+    </h1>
+  );
 }
 
 export default Home;
